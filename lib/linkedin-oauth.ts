63d389deaee8d12ebea407d32117e5a38a6d58df
Gulnoza U <gulnozausmon0708@gmail.com>
2026-05-05 20:46:59 -0700

    changed callback

--- lib/linkedin-oauth.ts
+++ lib/linkedin-oauth.ts
@@ -11,7 +11,7 @@ export function getLinkedInRedirectUri(): string {
   if (typeof window !== "undefined") {
     uri = `${window.location.origin}/api/auth/linkedin/callback`;
   } else {
-    uri = "http://localhost:3001/api/auth/linkedin/callback";
+    uri = "https://nect-connect.vercel.app/api/auth/linkedin/callback";
   }
 
   console.log("DEBUG: Sending Redirect URI to LinkedIn ->", uri);
