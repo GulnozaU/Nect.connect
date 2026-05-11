18005df57cc9ba37f6c0bb31147a9f11f7a85eaf
Gulnoza U <gulnozausmon0708@gmail.com>
2026-05-11 15:59:57 -0700

    updated the speed of the demo

--- lib/demo-video-chapters.ts
+++ lib/demo-video-chapters.ts
@@ -1,8 +1,7 @@
+export const DEMO_VIDEO_PATH_MP4 = "/Nect_demo.mp4";
 
-export const DEMO_VIDEO_PATH_MP4 = "/demo_YC-S26.mp4";
 
-
-export const DEMO_VIDEO_PATH_MOV = "/demo_YC-S26.mp4";
+export const DEMO_VIDEO_PATH_MOV = "/Nect_demo.mp4";
 
 
 export const DEMO_VIDEO_PLAYBACK_RATE = 2;
